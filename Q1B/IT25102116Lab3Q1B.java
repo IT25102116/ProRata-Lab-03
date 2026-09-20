15c2203887150b7b602877a627f68543bcab3d5a
IT25102116 <it25102116@my.sliit.lk>
2026-09-20 21:44:58 +0530

    Add Lab3Q1B

---
+++ Q1B/IT25102116Lab3Q1B.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class IT25102116Lab3Q1B {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter price of 1kg rice: ");
+        double price = input.nextDouble();
+
+        System.out.print("Enter number of kilograms: ");
+        double kilograms = input.nextDouble();
+
+        double amount = price * kilograms;
+
+        double discount = amount * 10 / 100;
+
+        double finalAmount = amount - discount;
+
+        System.out.println("Amount before discount = " + amount);
+        System.out.println("Discount = " + discount);
+        System.out.println("Amount to pay = " + finalAmount);
+    }
+}
